3c71c927680be802fd7bdcfc99fe62ba549de391
program think backup <103631137+hello-world-1989@users.noreply.github.com>
2024-03-29 17:22:29 +1300

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,2 +1,2 @@
-wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/android/android-tor.zip
-wget https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/android/android-freebrowser.zip
+wget -O android-tor.zip https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/android/android-tor.zip
+wget -O android-freebrowser.zip https://end-gfw.s3.ap-northeast-2.amazonaws.com/main/android/android-freebrowser.zip
